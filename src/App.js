daf1f82c658eb6350732b4803c223aea7506c267
NGabrielHrz <rotsen.soher@gmail.com>
2023-06-30 16:27:28 -0600

    Componentes y render primera versión

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import "./App.css";
+import React, { Component } from "react";
 
-const nombres = ["Néstor", "Leslie", "Sophia", "Arianna", "Gabriel"];
+// const App = () => <h1>Hello every nyas</h1>;
 
-function getNombres() {
-  const elementList = [];
-  for (let i = 0; i < nombres.length; i++) {
-    elementList.push(<li>{nombres[i]}</li>);
+class App extends Component {
+  state = {};
+  render() {
+    return <h1>Hello every nya, how are you</h1>;
   }
-  return elementList;
 }
-
-const Nombres = () => {
-  return (
-    <ul>
-      {nombres.map((nombre, index) => (
-        <li key={index}>{nombre}</li>
-      ))}
-    </ul> // un .map requiere un identificador key, con una porpoiedad de tipo index para una mayor optimización
-  );
-};
-
-const App = () => {
-  return (
-    <div>
-      <Nombres />
-    </div>
-  );
-};
 
 export default App;
